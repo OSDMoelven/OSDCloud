5c4d3b4b5c3347cfd2feb37d11a8fa6a4cc7d882
thufr <108275076+thufr@users.noreply.github.com>
2024-02-08 16:03:23 +0100

    Update Win 11 22H2 Pro - nb-NO.ps1

--- Win 11 22H2 Pro - nb-NO.ps1
+++ Win 11 22H2 Pro - nb-NO.ps1
@@ -1,16 +1,16 @@
-﻿Write-Host  -ForegroundColor Cyan 'Windows 11 22H2 Pro Autopilot nb-NO'
+﻿Write-Host -ForegroundColor Cyan 'Windows 11 22H2 Pro Autopilot nb-NO'
 #================================================
 #   [PreOS] Update Module
 #================================================
 if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Write-Host -ForegroundColor Green "Setting Display Resolution to 1600x"
     Set-DisRes 1600
 }
 
 Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
 Install-Module OSD -Force
 
-Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
 Import-Module OSD -Force   
 
 #=======================================================================
